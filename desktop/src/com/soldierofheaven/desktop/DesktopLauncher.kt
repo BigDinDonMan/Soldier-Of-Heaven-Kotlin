4e19ebf793dcae7705c91305828f00353204777f
BigDinDonMan <gorski.maciej@o2.pl>
2022-02-02 14:49:36 +0100

    initial commit

---
+++ desktop/src/com/soldierofheaven/desktop/DesktopLauncher.kt
@@ -0,0 +1,16 @@
+package com.soldierofheaven.desktop
+
+import kotlin.jvm.JvmStatic
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.soldierofheaven.SoldierOfHeavenGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration().apply {
+            title = "Soldier of Heaven"
+        }
+        LwjglApplication(SoldierOfHeavenGame(), config)
+    }
+}
