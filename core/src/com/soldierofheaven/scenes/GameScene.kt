e0efcd4c399fca5c31c0c6d372c50384c6c35b8b
BigDinDonMan <gorski.maciej@o2.pl>
2022-02-02 22:55:28 +0100

    added health bar to game scene

--- core/src/com/soldierofheaven/scenes/GameScene.kt
+++ core/src/com/soldierofheaven/scenes/GameScene.kt
@@ -5,10 +5,12 @@ import com.badlogic.gdx.InputMultiplexer
 import com.badlogic.gdx.ScreenAdapter
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.utils.viewport.StretchViewport
 import com.soldierofheaven.SoldierOfHeavenGame
 import com.soldierofheaven.ecs.PlayerInputHandler
 import com.soldierofheaven.ecs.systems.RenderSystem
+import com.soldierofheaven.ui.HealthBar
 import com.soldierofheaven.util.EcsWorld
 import com.soldierofheaven.util.PhysicsWorld
 import com.soldierofheaven.util.heightF
@@ -27,6 +29,25 @@ class GameScene(private val game: SoldierOfHeavenGame, private val ecsWorld: Ecs
 
     private val inputHandler = PlayerInputHandler(ecsWorld.getSystem(EventSystem::class.java))
 
+    private val defaultSkin = Skin(Gdx.files.internal("skins/uiskin.json"))
+    private lateinit var healthBar: HealthBar
+
+    init {
+        initUi()
+    }
+
+    private fun initUi() {
+        val healthPad = 10f
+        val healthHeight = 50f
+        val healthWidth = 250f
+        healthBar = HealthBar(150, defaultSkin).apply {
+            width = healthWidth
+            height = healthHeight
+            setPosition(healthPad, Gdx.graphics.heightF() - healthHeight - healthPad)
+        }
+        stage.addActor(healthBar)
+    }
+
     override fun show() {
         Gdx.input.inputProcessor = InputMultiplexer(stage, inputHandler)
     }
@@ -40,6 +61,8 @@ class GameScene(private val game: SoldierOfHeavenGame, private val ecsWorld: Ecs
         ecsWorld.setDelta(delta)
         ecsWorld.process()
 
+        stage.act()
+        stage.draw()
         if (debug) {
             debugRenderer.render(physicsWorld, ecsWorld.getSystem(RenderSystem::class.java).spriteBatch.projectionMatrix)
         }
