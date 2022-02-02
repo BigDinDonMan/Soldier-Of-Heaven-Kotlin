9be227cfdff846918c27e1958789a70f603dfa22
BigDinDonMan <gorski.maciej@o2.pl>
2022-02-02 22:20:23 +0100

    finished healthbar implementation, added default skins

--- core/src/com/soldierofheaven/scenes/MenuScene.kt
+++ core/src/com/soldierofheaven/scenes/MenuScene.kt
@@ -1,11 +1,14 @@
 package com.soldierofheaven.scenes
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
 import com.badlogic.gdx.ScreenAdapter
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.utils.viewport.StretchViewport
 import com.soldierofheaven.SoldierOfHeavenGame
+import com.soldierofheaven.ui.HealthBar
 import com.soldierofheaven.util.heightF
 import com.soldierofheaven.util.widthF
 import net.mostlyoriginal.api.event.common.Event
@@ -16,14 +19,23 @@ class MenuScene(private val game: SoldierOfHeavenGame) : ScreenAdapter() {
     private val viewport = StretchViewport(Gdx.graphics.widthF(), Gdx.graphics.heightF())
     private val stage = Stage(viewport)
 
+    private val defaultSkin = Skin(Gdx.files.internal("skins/uiskin.json"))
+
+//    private val healthBar = HealthBar(150, defaultSkin)
+
     init {
         initUi()
     }
 
     private fun initUi() {
-        val rootWidget = Table()
-        rootWidget.setFillParent(true)
-        rootWidget.setDebug(true)
+//        val healthBarPadding = 10f
+//        val height = 50f
+//        val width = 250f
+//        stage.addActor(healthBar.apply {
+//            setPosition(healthBarPadding, Gdx.graphics.heightF() - healthBarPadding - height)
+//            this.width = width
+//            this.height = height
+//        })
     }
 
     @Subscribe
@@ -31,12 +43,18 @@ class MenuScene(private val game: SoldierOfHeavenGame) : ScreenAdapter() {
 
     }
 
+    override fun show() {
+        Gdx.input.inputProcessor = stage
+    }
+
     override fun dispose() {
         stage.dispose()
+//        healthBar.dispose()
     }
 
     override fun render(delta: Float) {
-        super.render(delta)
+        stage.act()
+        stage.draw()
     }
 
     override fun resize(width: Int, height: Int) = viewport.update(width, height)
--- core/src/com/soldierofheaven/ui/HealthBar.kt
+++ core/src/com/soldierofheaven/ui/HealthBar.kt
@@ -1,8 +1,57 @@
 package com.soldierofheaven.ui
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.utils.Disposable
 
-class HealthBar : Actor() {
+class HealthBar(private val maxHealth: Int, labelSkin: Skin) : Actor(), Disposable {
+    private var displayValue = maxHealth
+        set(value){
+            field = value
+            label.setText("${displayValue}/${maxHealth}")
+            widthMultiplier = displayValue.toFloat() / maxHealth
+            println("${widthMultiplier}, ${displayValue}, ${maxHealth}")
+        }
+    private val label = Label("${displayValue}/${maxHealth}", labelSkin)
+    private val shapeRenderer = ShapeRenderer()
 
+    private var widthMultiplier: Float = displayValue.toFloat() / maxHealth
 
+    var borderWidth = 2
+
+    fun updateDisplay(newHealth: Int) {
+        displayValue = newHealth
+    }
+
+    override fun act(delta: Float) {
+        super.act(delta)
+        label.act(delta)
+        val midX = (x + width) / 2
+        val lx = midX - label.width / 2
+        val ly = y + height / 2 - label.height / 2
+        label.setPosition(lx, ly)
+    }
+
+    override fun draw(batch: Batch, parentAlpha: Float) {
+        Gdx.gl.glLineWidth(borderWidth.toFloat())
+        batch.end()
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line)
+        shapeRenderer.rect(x, y, width, height, Color.RED, Color.RED, Color.RED, Color.RED)
+        shapeRenderer.end()
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled)
+        shapeRenderer.rect((x + borderWidth / 2), y + borderWidth / 2, (width - borderWidth) * widthMultiplier, height - borderWidth, Color.RED, Color.GREEN, Color.GREEN, Color.RED)
+        shapeRenderer.end()
+        batch.begin()
+        label.draw(batch, parentAlpha)
+    }
+
+    override fun dispose() {
+        shapeRenderer.dispose()
+    }
 }
