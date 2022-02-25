da48aa3839f9c9db24d0fe3cf5edaf15ed1bbe93
BigDinDonMan <gorski.maciej@o2.pl>
2022-02-25 19:13:07 +0100

    added prefab class and player position vector reference to enemy components

--- core/src/com/soldierofheaven/ecs/components/enemies/RangedEnemy.kt
+++ core/src/com/soldierofheaven/ecs/components/enemies/RangedEnemy.kt
@@ -2,8 +2,13 @@ package com.soldierofheaven.ecs.components.enemies
 
 import com.artemis.PooledComponent
 import com.badlogic.gdx.ai.btree.BehaviorTree
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.math.Vector3
 
 class RangedEnemy : PooledComponent() {
+
+    var playerPosition: Vector3? = null
+
     override fun reset() {
 
     }
---
+++ core/src/com/soldierofheaven/prototypes/Prefab.kt
@@ -0,0 +1,8 @@
+package com.soldierofheaven.prototypes
+
+import com.soldierofheaven.util.EcsWorld
+import com.soldierofheaven.util.PhysicsWorld
+
+abstract class Prefab(private val ecsWorld: EcsWorld, private val physicsWorld: PhysicsWorld) {
+    abstract fun instantiate()
+}
